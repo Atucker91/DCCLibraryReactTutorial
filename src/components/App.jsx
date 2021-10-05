e5fd39c35494318cc74d1b4cdaa1e89d87cf18ad
Atucker91 <89651874+Atucker91@users.noreply.github.com>
2021-10-05 15:23:00 -0500

    split book view logic into its own component file BookViewer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import TitleBar from './TitleBar/TitleBar';
+import BookViewer from './BookViewer/BookViewer';
 
 class App extends Component {
     constructor(props){
@@ -43,21 +44,7 @@ class App extends Component {
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="row">
-                    <div className="col-md-4">
-                        {/*Button here to move to the previous book viewed*/}
-                        <button onClick={this.gotToPreviousBook}>Previous Book</button>
-                    </div>
-                    <div className="col-md-4">
-                        {/*Display book with cover*/}
-                        <h1>{this.books[this.state.bookNumber].title}</h1>
-                        <h4>{this.books[this.state.bookNumber].author}</h4>
-                    </div>
-                    <div className="col-md-4">
-                        {/*Button here to move to the next book viewed*/}
-                        <button onClick={this.gotToNextBook}>Next Book</button>
-                    </div>
-                </div>
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.gotToNextBook} previousBook={this.gotToPreviousBook} />
             </div>
         )
     }
